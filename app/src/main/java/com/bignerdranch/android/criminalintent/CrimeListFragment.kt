825c28e1b934397fd98838ac578e9bdda04a920d
Fuyu <1249758797@qq.com>
2023-06-13 10:01:10 +0800

    10 使用布局与部件创建用户界面

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.criminalintent
 
+import android.media.Image
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -65,6 +68,7 @@ class CrimeListFragment : Fragment() {
         // 在构造函数里生成视图
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
 
         init {
             itemView.setOnClickListener(this)
@@ -73,7 +77,14 @@ class CrimeListFragment : Fragment() {
         fun bind(crime: Crime) {
             this.crime = crime
             titleTextView.text = this.crime.title
-            dateTextView.text = this.crime.date.toString()
+            dateTextView.text = DateFormat.format("yyyy MM dd,EEEE, h:mmaa", crime.date) // 格式化日期
+
+            // 控制手铐图片显示
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         // 检测用户点击事件
